93e39ff049e63db0bddc99cfdc55b0a588626d22
Ryan <ryanb0717@gmail.com>
2025-02-13 16:47:00 +0800

    Add AuditLog model for tracking user actions

---
+++ BookwormOnline/Models/AuditLog.cs
@@ -0,0 +1,13 @@
+﻿namespace BookwormOnline.Models
+{
+    public class AuditLog
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Action { get; set; }
+        public string Details { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string IpAddress { get; set; }
+        public string UserAgent { get; set; }
+    }
+}
