826e5d562d807104e03ffc7411a4e65a21a2f935
Ryan <ryanb0717@gmail.com>
2025-02-13 16:47:14 +0800

    Add EmailService for sending emails asynchronously with SMTP configuration

---
+++ BookwormOnline/Services/EmailService.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.Mail;
+using System.Net;
+
+namespace BookwormOnline.Services
+{
+    public interface IEmailService
+    {
+        Task SendEmailAsync(string to, string subject, string body);
+    }
+
+    public class EmailService : IEmailService
+    {
+        private readonly IConfiguration _configuration;
+
+        public EmailService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task SendEmailAsync(string to, string subject, string body)
+        {
+            using var client = new SmtpClient(_configuration["Email:SmtpHost"], int.Parse(_configuration["Email:SmtpPort"]))
+            {
+                EnableSsl = true,
+                Credentials = new NetworkCredential(_configuration["Email:Username"], _configuration["Email:Password"])
+            };
+
+            var mailMessage = new MailMessage
+            {
+                From = new MailAddress(_configuration["Email:FromAddress"]),
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true
+            };
+            mailMessage.To.Add(to);
+
+            await client.SendMailAsync(mailMessage);
+        }
+    }
+}
