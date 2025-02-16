01053ca26b8c61d420b7ebcd43ea0a78b6e8256c
Ryan <ryanb0717@gmail.com>
2025-02-16 20:51:56 +0800

    Add HTTPS support to Vite server with local certificate files

--- BookwormOnline Frontend/bookwormonline-frontend/vite.config.js
+++ BookwormOnline Frontend/bookwormonline-frontend/vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import fs from 'fs'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000
+    port: 3000,
+    https: {
+      pfx: fs.readFileSync(path.resolve(__dirname, 'localhost.pfx')),
+      passphrase: 'password'
+    }
   }
 })
